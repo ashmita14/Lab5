b6e9df3d9e9f351e2c2dbfbc453e0a021194c1ab
ashmita14 <42172528+ashmita14@users.noreply.github.com>
2018-08-31 15:54:26 +0530

    Add files via upload

---
+++ lab5_q12.cpp
@@ -0,0 +1,66 @@
+//Conditional program
+//Write a program to input month number and print number of days in that month.
+
+//including libraries
+#include<iostream>
+using namespace std;
+//declaring main function
+int main(){
+	//declaring variables
+	int i;
+	//asking user for month number
+	cout <<"Enter month number."<<endl;
+	//accepting value
+	cin >>i;
+	//checking week day number 
+	if(i==1){
+		//displaying result
+	cout <<"Number of days is 31."<<endl;
+	}
+	if(i==2){
+		//displaying result
+	cout <<"Number of days is 28."<<endl;
+	}
+	if(i==3){
+		//displaying result
+	cout <<"Number of days is 31."<<endl;
+	}
+	if(i==4){
+		//displaying result
+	cout <<"Number of days is 30."<<endl;
+	}
+	if(i==5){
+		//displaying result
+	cout <<"Number of days is 31."<<endl;
+	}
+	if(i==6){
+		//displaying result
+	cout <<"Number of days is 30."<<endl;
+	}
+	if(i==7){
+		//displaying result
+	cout <<"Number of days is 31."<<endl;
+	}
+	if(i==8){
+		//displaying result
+	cout <<"Number of days is 31."<<endl;
+	}
+	if(i==9){
+		//displaying result
+	cout <<"Number of days is 30."<<endl;
+	}
+	if(i==10){
+		//displaying result
+	cout <<"Number of days is 31."<<endl;
+	}
+	if(i==11){
+		//displaying result
+	cout <<"Number of days is 30."<<endl;
+	}
+	if(i==12){
+		//displaying result
+	cout <<"Number of days is 31."<<endl;
+	}
+	//returning integer value to main function
+	return 0;
+}
